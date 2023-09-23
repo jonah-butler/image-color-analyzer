53dc38761a68f878a29687847544dfd0ccfd51eb
Jonah Butler <jonahbutler6@gmail.com>
2023-09-23 14:05:22 -0400

    median cut algo - first pass

--- dist/index.js
+++ dist/index.js
@@ -8,76 +8,157 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-// const IMAGE = new Image();
-// const CANVAS: HTMLCanvasElement;
-// const CONTEXT = CANVAS.getContext("2d");
-var IMAGE, CANVAS, CONTEXT;
+let IMAGE, CANVAS, CONTEXT;
+const SIZE_DIVIDEND = 50;
+const DUMMY_DATA = [
+    {
+        r: 55,
+        g: 109,
+        b: 204,
+        a: 255,
+    },
+    {
+        r: 128,
+        g: 86,
+        b: 135,
+        a: 255,
+    },
+    {
+        r: 69,
+        g: 56,
+        b: 100,
+        a: 255,
+    },
+    {
+        r: 125,
+        g: 131,
+        b: 224,
+        a: 255,
+    },
+    {
+        r: 187,
+        g: 153,
+        b: 223,
+        a: 255,
+    },
+    {
+        r: 221,
+        g: 126,
+        b: 151,
+        a: 255,
+    },
+    {
+        r: 246,
+        g: 185,
+        b: 205,
+        a: 255,
+    },
+    {
+        r: 21,
+        g: 21,
+        b: 39,
+        a: 255,
+    },
+];
 // just a dummy src for now
-var SRC = "https://dev-blog-resources.s3.amazonaws.com/canvas_1691764016404.png";
-var initialize = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var data, cd;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                IMAGE = new Image();
-                CANVAS = document.createElement("canvas");
-                CONTEXT = CANVAS.getContext("2d");
-                IMAGE.crossOrigin = "anonymous";
-                IMAGE.src = SRC;
-                // load image
-                return [4 /*yield*/, IMAGE.decode()];
-            case 1:
-                // load image
-                _a.sent();
-                setCanvasHeightAndWidth();
-                console.log("IMAGE: ", IMAGE);
-                CONTEXT === null || CONTEXT === void 0 ? void 0 : CONTEXT.drawImage(IMAGE, 0, 0, CANVAS.width, CANVAS.height);
-                data = CONTEXT === null || CONTEXT === void 0 ? void 0 : CONTEXT.getImageData(0, 0, CANVAS.width, CANVAS.height).data;
-                if (data) {
-                    cd = medianColorBlend(data);
-                    console.log("rgba(".concat(cd.r, ", ").concat(cd.g, ", ").concat(cd.b, ", ").concat(cd.a, ")"));
-                }
-                return [2 /*return*/];
-        }
-    });
-}); };
-var setCanvasHeightAndWidth = function () {
-    CANVAS.width = IMAGE.width;
-    CANVAS.height = IMAGE.height;
+const SRC = "https://dev-blog-resources.s3.amazonaws.com/wavy-yellow-compressed2.jpg";
+const initialize = () => __awaiter(void 0, void 0, void 0, function* () {
+    IMAGE = new Image();
+    CANVAS = document.createElement("canvas");
+    CONTEXT = CANVAS.getContext("2d");
+    IMAGE.crossOrigin = "anonymous";
+    IMAGE.src = SRC;
+    // load image
+    yield IMAGE.decode();
+    setCanvasHeightAndWidth();
+    CONTEXT === null || CONTEXT === void 0 ? void 0 : CONTEXT.drawImage(IMAGE, 0, 0, CANVAS.width, CANVAS.height);
+    const data = CONTEXT === null || CONTEXT === void 0 ? void 0 : CONTEXT.getImageData(0, 0, CANVAS.width, CANVAS.height).data;
+    if (data && data.length) {
+        const rgba = getRGBValues(data);
+        const palette = quanitizeRGB(rgba);
+        console.log("palette: ", palette);
+    }
+    else {
+        throw new Error("image data unavailable");
+    }
+});
+const setCanvasHeightAndWidth = () => {
+    CANVAS.width = IMAGE.width / SIZE_DIVIDEND;
+    CANVAS.height = IMAGE.height / SIZE_DIVIDEND;
 };
-var medianColorBlend = function (d) {
-    var r = 0;
-    var g = 0;
-    var b = 0;
-    var a = 255;
-    for (var i = 0; i < d.length; i += 4) {
+const quanitizeRGB = (d, startingDepth = 0, maxDepth = 2) => {
+    if (startingDepth === maxDepth) {
+        const color = d.reduce((p, c) => {
+            p.r += c.r;
+            p.g += c.g;
+            p.b += c.b;
+            return p;
+        }, { r: 0, g: 0, b: 0, a: 255 });
+        return [
+            {
+                r: Math.round(color.r / d.length),
+                g: Math.round(color.g / d.length),
+                b: Math.round(color.b / d.length),
+                a: 255,
+            }
+        ];
+    }
+    const dominantColor = findDominantColorRange(d);
+    sortByDominantColor(d, dominantColor);
+    const middle = d.length / 2;
+    return [
+        ...quanitizeRGB(d.slice(0, middle), startingDepth + 1, maxDepth),
+        ...quanitizeRGB(d.slice(middle), startingDepth + 1, maxDepth)
+    ];
+};
+const sortByDominantColor = (d, key) => {
+    d.sort((p1, p2) => {
+        return p1[key] - p2[key];
+    });
+};
+const getRGBValues = (d) => {
+    const rgbaVals = [];
+    for (let i = 0; i < d.length; i += 4) {
+        rgbaVals.push({
+            r: d[i],
+            g: d[i + 1],
+            b: d[i + 2],
+            a: 255,
+        });
+    }
+    return rgbaVals;
+};
+const findDominantColorRange = (rgbaRecords) => {
+    const d = rgbaRecords[0];
+    let rMin = d.r, rMax = d.r;
+    let gMin = d.g, gMax = d.g;
+    let bMin = d.b, bMax = d.b;
+    rgbaRecords.forEach((pixel) => {
+        rMin = Math.min(rMin, pixel.r);
+        gMin = Math.min(gMin, pixel.g);
+        bMin = Math.min(bMin, pixel.b);
+        rMax = Math.max(rMax, pixel.r);
+        gMax = Math.max(gMax, pixel.g);
+        bMax = Math.max(bMax, pixel.b);
+    });
+    const rRange = rMax - rMin;
+    const gRange = gMax - gMin;
+    const bRange = bMax - bMin;
+    const maxRange = Math.max(rRange, gRange, bRange);
+    if (maxRange === rRange) {
+        return "r";
+    }
+    else if (maxRange === gRange) {
+        return "g";
+    }
+    return "b";
+};
+const medianColorBlend = (d) => {
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    const a = 255;
+    for (let i = 0; i < d.length; i += 4) {
         r += d[i];
         g += d[i + 1];
         b += d[i + 2];
@@ -87,10 +168,48 @@ var medianColorBlend = function (d) {
     g = Math.floor(g / d.length);
     b = Math.floor(b / d.length);
     return {
-        r: r,
-        g: g,
-        b: b,
-        a: a,
+        r,
+        g,
+        b,
+        a,
+    };
+};
+;
+const dominantColor = (d) => {
+    const totals = {};
+    let r = 0;
+    let g = 0;
+    let b = 0;
+    const a = 255;
+    for (let i = 0; i < d.length - 1; i += 4) {
+        r += d[i];
+        g += d[i + 1];
+        b += d[i + 2];
+        const key = `${d[i]}_${d[i + 1]}_${d[i + 2]}_${a}`;
+        if (totals[key] === undefined) {
+            totals[key] = 1;
+        }
+        else {
+            totals[key] += 1;
+        }
+    }
+    let largest = 0;
+    let rgba = "";
+    for (const c of Object.keys(totals)) {
+        if (totals[c] > largest) {
+            largest = totals[c];
+            rgba = c;
+        }
+    }
+    const split = rgba.split("_");
+    r = parseInt(split[0]);
+    g = parseInt(split[1]);
+    b = parseInt(split[2]);
+    return {
+        r,
+        g,
+        b,
+        a,
     };
 };
 initialize();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,60 @@
-// const IMAGE = new Image();
-// const CANVAS: HTMLCanvasElement;
-// const CONTEXT = CANVAS.getContext("2d");
 let IMAGE: HTMLImageElement,
   CANVAS: HTMLCanvasElement,
   CONTEXT: CanvasRenderingContext2D | null;
+
+const SIZE_DIVIDEND = 50;
+
+
+const DUMMY_DATA: RGBARecord[] = [
+  {
+    r: 55,
+    g: 109,
+    b: 204,
+    a: 255,
+  },
+  {
+    r: 128,
+    g: 86,
+    b: 135,
+    a: 255,
+  },
+  {
+    r: 69,
+    g: 56,
+    b: 100,
+    a: 255,
+  },
+  {
+    r: 125,
+    g: 131,
+    b: 224,
+    a: 255,
+  },
+  {
+    r: 187,
+    g: 153,
+    b: 223,
+    a: 255,
+  },
+  {
+    r: 221,
+    g: 126,
+    b: 151,
+    a: 255,
+  },
+  {
+    r: 246,
+    g: 185,
+    b: 205,
+    a: 255,
+  },
+  {
+    r: 21,
+    g: 21,
+    b: 39,
+    a: 255,
+  },
+]
 
 interface RGBARecord {
   r: number;
@@ -14,7 +65,7 @@ interface RGBARecord {
 
 // just a dummy src for now
 const SRC =
-  "https://dev-blog-resources.s3.amazonaws.com/canvas_1691764016404.png";
+  "https://dev-blog-resources.s3.amazonaws.com/wavy-yellow-compressed2.jpg";
 
 const initialize = async (): Promise<void> => {
   IMAGE = new Image();
@@ -30,20 +81,109 @@ const initialize = async (): Promise<void> => {
 
   setCanvasHeightAndWidth();
 
-  console.log("IMAGE: ", IMAGE);
 
   CONTEXT?.drawImage(IMAGE, 0, 0, CANVAS.width, CANVAS.height);
   const data = CONTEXT?.getImageData(0, 0, CANVAS.width, CANVAS.height).data;
-  if (data) {
-    const cd = medianColorBlend(data);
-    console.log(`rgba(${cd.r}, ${cd.g}, ${cd.b}, ${cd.a})`);
+  if (data && data.length) {
+  
+    const rgba = getRGBValues(data);
+    const palette = quanitizeRGB(rgba);
+    console.log("palette: ", palette);
+  } else {
+   throw new Error("image data unavailable");
   }
 };
 
 const setCanvasHeightAndWidth = (): void => {
-  CANVAS.width = IMAGE.width;
-  CANVAS.height = IMAGE.height;
+  CANVAS.width = IMAGE.width / SIZE_DIVIDEND;
+  CANVAS.height = IMAGE.height / SIZE_DIVIDEND;
 };
+
+const quanitizeRGB = (d: RGBARecord[], startingDepth = 0, maxDepth = 2): RGBARecord[] => {
+  if(startingDepth === maxDepth) {
+    const color = d.reduce((p, c): RGBARecord => {
+      p.r += c.r;
+      p.g += c.g;
+      p.b += c.b;
+      return p;
+    }, { r: 0, g: 0, b: 0, a: 255 });
+
+
+    return [
+      {
+        r: Math.round(color.r / d.length),
+        g: Math.round(color.g / d.length),
+        b: Math.round(color.b / d.length),
+        a: 255,
+      }
+    ]
+
+  }
+
+  const dominantColor = findDominantColorRange(d);
+  
+  sortByDominantColor(d, dominantColor);
+
+  const middle = d.length / 2;
+
+  return [
+    ...quanitizeRGB(d.slice(0, middle), startingDepth + 1, maxDepth),
+    ...quanitizeRGB(d.slice(middle), startingDepth + 1, maxDepth)
+  ]
+
+}
+
+const sortByDominantColor = (d: RGBARecord[], key: string): void => {
+  d.sort((p1, p2) => {
+    return p1[key as keyof RGBARecord] - p2[key as keyof RGBARecord];
+  });
+}
+
+const getRGBValues = (d: Uint8ClampedArray): RGBARecord[] => {
+  const rgbaVals: RGBARecord[] = [];
+  for (let i = 0; i < d.length; i += 4) {
+    rgbaVals.push(
+      {
+        r: d[i],
+        g: d[i + 1],
+        b: d[i + 2],
+        a: 255,
+      }
+    );
+  }
+  return rgbaVals;
+};
+
+
+const findDominantColorRange = (rgbaRecords: RGBARecord[]): string => {
+  const d = rgbaRecords[0];
+  let rMin = d.r, rMax = d.r;
+  let gMin = d.g, gMax = d.g;
+  let bMin = d.b, bMax = d.b;
+
+  rgbaRecords.forEach((pixel): void => {
+    rMin = Math.min(rMin, pixel.r);
+    gMin = Math.min(gMin, pixel.g);
+    bMin = Math.min(bMin, pixel.b);
+
+    rMax = Math.max(rMax, pixel.r);
+    gMax = Math.max(gMax, pixel.g);
+    bMax = Math.max(bMax, pixel.b);
+  });
+
+  const rRange = rMax - rMin;
+  const gRange = gMax - gMin;
+  const bRange = bMax - bMin;
+
+  const maxRange = Math.max(rRange, gRange, bRange);
+  
+  if (maxRange === rRange) {
+    return "r";
+  } else if (maxRange === gRange) {
+  return "g";
+  }
+  return "b";
+}
 
 const medianColorBlend = (d: Uint8ClampedArray): RGBARecord => {
   let r = 0;
@@ -67,5 +207,46 @@ const medianColorBlend = (d: Uint8ClampedArray): RGBARecord => {
     a,
   };
 };
+
+interface RGBATotals {
+  [key: string]: number;
+};
+
+const dominantColor = (d: Uint8ClampedArray): RGBARecord => {
+  const totals: RGBATotals = {};
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  const a = 255;
+  for (let i = 0; i < d.length - 1; i += 4) {
+    r += d[i];
+    g += d[i + 1];
+    b += d[i + 2];
+    const key = `${d[i]}_${d[i + 1]}_${d[i + 2]}_${a}`;
+    if(totals[key] === undefined) {
+      totals[key] = 1;
+    } else {
+      totals[key] += 1;
+    }
+  }
+  let largest = 0;
+  let rgba = "";
+  for(const c of Object.keys(totals)) {
+    if(totals[c] > largest) {
+      largest = totals[c];
+      rgba = c;
+    }
+  }
+  const split = rgba.split("_");
+  r = parseInt(split[0]);
+  g = parseInt(split[1]);
+  b = parseInt(split[2]);
+  return {
+    r,
+    g,
+    b,
+    a,
+  };
+}
 
 initialize();
