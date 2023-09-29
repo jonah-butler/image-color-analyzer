13295e4c54ebadfc60ea6ddcc2dd24770fdc2abf
jonah-butler <jonahbutler6@gmail.com>
2023-09-29 18:54:12 -0400

    updates, comments and a readme

---
+++ README.md
@@ -0,0 +1,228 @@
+# Palette
+
+Generate various RGBA color palettes based on image pixel data
+
+## Installing
+
+```shell
+npm i @jayimbee/palette
+```
+
+## Example
+
+```typescript
+import palette from @jaimbee/palette;
+
+
+const imgData = palette.extractImageDataFromSrc('https://some.site.com/godzilla.png', 3);
+
+const imageColorPalette = palette.quantize(imgData);
+
+console.log(imageColorPalette);
+//
+//     [
+//      {
+//        r: 123,
+//        g: 145,
+//        b: 12,
+//        a: 255
+//       }
+//       ...
+//     ]
+```
+
+# Methods
+
+### 🎨 `blend`
+
+Type: `Function`
+
+Description: _returns a single blend of all pixel color values_
+
+⚙️ Params
+
+- d `<Uint8ClampedArray>`: value returned from a canvas context calling `.getImageData().data`
+
+### 📦 Returns
+
+```typescript
+<RGBARecord>
+
+interface RGBARecord {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+```
+
+### 🛠️ Usage
+
+```typescript
+const imgData = palette.extractImageDataFromSrc(imgURL, 3);
+
+palette.blend(imgData);
+```
+
+---
+
+### 🎨 `dominant`
+
+Type: `Function`
+
+Description: _returns the most reoccurring pixel color_
+
+⚙️ Params
+
+- d `<Uint8ClampedArray>`: value returned from a canvas context calling `.getImageData().data`
+
+### 🛠️ Usage
+
+```typescript
+const imgData = palette.extractImageDataFromSrc(imgURL, 3);
+
+palette.dominant(imgData);
+```
+
+### 📦 Returns
+
+```typescript
+<RGBARecord>
+
+interface RGBARecord {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+```
+
+---
+
+### 🎨 `quantize`
+
+Type: `Function`
+
+Description: _using the median cut algorithm, this returns an array of colors selected through finding the dominant color range and quantizing the color sets until the provided max depth is reached_
+
+⚙️ Params
+
+- d `<Uint8ClampedArray>`
+  - value returned from a canvas context calling `.getImageData().data`
+- startingDepth `<number>`
+  - _default set to 0_
+- maxDepth `<number>`
+  - _default set to 2_
+
+### 🛠️ Usage
+
+```typescript
+const imgData = palette.extractImageDataFromSrc(imgURL, 3);
+
+palette.quantize(imgData);
+```
+
+### 📦 Returns
+
+```typescript
+<RGBARecord>[];
+
+interface RGBARecord {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+```
+
+---
+
+### 🎨 `extractImageDataFromSrc`
+
+Type: `Function`
+
+Description: _returns the most reocurring pixel color_
+
+⚙️ Params
+
+- src `<string>`
+  - an image src
+- sizeDividend `<number>`
+
+  - default set to `1`
+  - this is primarily for making the median cut algorithm more performant by reducing image size while keeping aspect ration in tact. Very large images require a lot of processing, so supplying a size dividend can speed up this palette generating process while keeping the final palette that is generated mostly unaffected within reason.
+  - A custom implementation can utilize a size dividend by dividing the `CANVAS.width` and `CANVAS.height` by some number:
+
+  ```typescript
+  const IMAGE = new Image();
+  const CANVAS = document.createElement("canvas");
+
+  IMAGE.src = src;
+
+  await IMAGE.decode();
+
+  CANVAS.width = IMAGE.width / sizeDividend;
+  CANVAS.height = IMAGE.height / sizeDividend;
+  ```
+
+🚨 Calling `getImageData` on an Image that's loaded with a source that is cross-origin is known to create CORS issues via the _tainted canvas_ error. This helper is here to simplify the process of getting image data, but a custom implementation of this may be a better solution for some. Things to note with this function is the resource server handling the requested image must include the response header: `Access-Control-Allow-Origin`.
+
+🛑 If `.quantize()` is running too slow, reduce the size of the image as show above
+
+### 🛠️ Usage
+
+```typescript
+palette.extractImageDataFromSrc(imgData);
+```
+
+### 📦 Returns
+
+```
+<Uint8ClampedArray>
+```
+
+---
+
+### 🎨 `monochromatic`
+
+Type: `Function`
+
+Description:_returns a monochromatic object with colors ranginng in a spectrum from dark to light_
+
+⚙️ Params
+
+- d `<Uint8ClampedArray>`
+  - value returned from a canvas context calling `.getImageData().data`
+- numOfColors: <number>
+  - default set to `4`
+  - the amount of returned monochromatic colors
+- rgb: `{r: number, g: number, b: number}`
+
+  - an object containing the fields `r, g, b`
+
+### 🛠️ Usage
+
+```typescript
+const imgData = palette.extractImageDataFromSrc(imgURL, 3);
+
+palette.monochromatic(imgData);
+```
+
+### 📦 Returns
+
+```typescript
+<MonoChromatic>
+
+interface RGBARecord {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+
+interface MonoChromatic {
+  light: RGBARecord[];
+  dark: RGBARecord[];
+  original: RGBARecord;
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,43 @@ interface RGBARecord {
   g: number;
   b: number;
   a: number;
-};
+}
 
 interface RGBATotals {
   [key: string]: number;
-};
+}
 
 interface MonoChromatic {
-  light: RGBARecord[],
-  dark: RGBARecord[],
-  original: RGBARecord,
-};
+  light: RGBARecord[];
+  dark: RGBARecord[];
+  original: RGBARecord;
+}
 
 const palette = {
-
-  getMedianColorBlend(d: Uint8ClampedArray): RGBARecord {
+  /**
+   *
+   * @param d <Uint8ClampedArray>
+   *        The array values returned from calling getImageData on canvas context
+   * @returns <RGBARecord>
+   *          A median blend of each pixels rgb value
+   */
+  blend(d: Uint8ClampedArray): RGBARecord {
     let r = 0;
     let g = 0;
     let b = 0;
+
     const a = 255;
+
     for (let i = 0; i < d.length; i += 4) {
       r += d[i];
       g += d[i + 1];
       b += d[i + 2];
     }
-    console.log(r, g, b);
-  
+
     r = Math.floor(r / d.length);
     g = Math.floor(g / d.length);
     b = Math.floor(b / d.length);
+
     return {
       r,
       g,
@@ -40,35 +48,53 @@ const palette = {
     };
   },
 
-  getDominantColor(d: Uint8ClampedArray): RGBARecord {
+  /**
+   *
+   * @param d <Uint8ClampedArray>
+   *        The array values returned from calling getImageData on canvas context
+   * @returns <RGBARecord>
+   *          The rgba values of the most dominant pixel in the provided image
+   */
+  dominant(d: Uint8ClampedArray): RGBARecord {
     const totals: RGBATotals = {};
-    let r = 0;
-    let g = 0;
-    let b = 0;
+
+    let r = 0,
+      g = 0,
+      b = 0;
+
     const a = 255;
+
+    // get each pixel and set each RGB value
+    // as the key in the totals record and
+    // store running total of each instance of
+    // that pixel color
     for (let i = 0; i < d.length - 1; i += 4) {
       r += d[i];
       g += d[i + 1];
       b += d[i + 2];
+
       const key = `${d[i]}_${d[i + 1]}_${d[i + 2]}_${a}`;
-      if(totals[key] === undefined) {
-        totals[key] = 1;
-      } else {
-        totals[key] += 1;
-      }
+
+      totals[key] != undefined ? (totals[key] += 1) : 1;
     }
+
     let largest = 0;
     let rgba = "";
-    for(const c of Object.keys(totals)) {
-      if(totals[c] > largest) {
+
+    // find the largest in the totals record
+    for (const c of Object.keys(totals)) {
+      if (totals[c] > largest) {
         largest = totals[c];
         rgba = c;
       }
     }
+
     const split = rgba.split("_");
+
     r = parseInt(split[0]);
     g = parseInt(split[1]);
     b = parseInt(split[2]);
+
     return {
       r,
       g,
@@ -77,13 +103,50 @@ const palette = {
     };
   },
 
-  getColorPalette(d: Uint8ClampedArray, startingDepth = 0, maxDepth = 2): RGBARecord[] {
+  /**
+   *
+   * @param d <Uint8ClampedArray>
+   *        The array values returned from calling getImageData on canvas context
+   * @param startingDepth <number>
+   *        the
+   * @param maxDepth
+   * @returns <RGBARecord[]>
+   */
+  quantize(
+    d: Uint8ClampedArray,
+    startingDepth = 0,
+    maxDepth = 2
+  ): RGBARecord[] {
+    if (startingDepth <= maxDepth) {
+      throw new Error(
+        "startingDepth param must be initialized to a value lower than the maxDepth parameter"
+      );
+    }
+
     const rgbaData = getRGBValues(d);
     return quantizeRGB(rgbaData, startingDepth, maxDepth);
   },
 
-  async extractImageDataFromSrc(src: string, sizeDividend: number = 1): Promise<Uint8ClampedArray | undefined | Error> {
-
+  /**
+   *
+   * Using this method can result in CORS issues, so using this
+   * pre-built implementation requires the resource server to have the
+   * Access-Control-Allow-Origin header present.
+   *
+   * @param src <string>
+   *        The image url
+   * @param sizeDividend <number>
+   *        The dividend used to resize(but keep aspection ratio in tact) the canvas before being drawn.
+   *        This should in most cases be used when the image being analyzed
+   *        is decently large(ie. > 1500px) as this will make the quantization
+   *        much more performant. Pixel data is sacrificed at the expense of speed
+   *        with the final generated palette being most unaffected - within reason.
+   * @returns image data <Uint8ClampedArray> | undefined | Error
+   */
+  async extractImageDataFromSrc(
+    src: string,
+    sizeDividend: number = 1
+  ): Promise<Uint8ClampedArray | undefined | Error> {
     const IMAGE = new Image();
     const CANVAS = document.createElement("canvas");
     const CONTEXT = CANVAS.getContext("2d");
@@ -92,88 +155,94 @@ const palette = {
     IMAGE.src = src;
 
     try {
-
       await IMAGE.decode();
 
       CANVAS.width = IMAGE.width / sizeDividend;
       CANVAS.height = IMAGE.height / sizeDividend;
-  
+
       CONTEXT?.drawImage(IMAGE, 0, 0, CANVAS.width, CANVAS.height);
-  
+
       return CONTEXT?.getImageData(0, 0, CANVAS.width, CANVAS.height).data;
-
     } catch {
-
       throw new Error(`Failed to decode the provided image src: ${src}`);
-
     }
-
   },
 
-  generateMonoChromatic(percent: number, colors: number = 4, rgb: {r: number, g: number, b: number}): MonoChromatic {
-    
-    const hsl = RGBToHSL(rgb.r, rgb.g, rgb. b);
-    
+  /**
+   *
+   * @param percent <number>
+   *        The percent used in shifting the Lightness value of a provided RGB value
+   * @param colors <number>
+   *        The amount of light and dark monochromatic values returned
+   * @param rgb {r,g,b: <number>}
+   *        The RGB values to which a monochromatic palette will be generated
+   * @returns <MonoChromatic>
+   *          light and dark keys includes an array of rgb color values equal
+   *          up to the number of colors provided in the param with the color's lightness
+   *          value calculated either up or down from the provided percent.
+   */
+  monochromatic(
+    percent: number,
+    numOfColors: number = 4,
+    rgb: { r: number; g: number; b: number }
+  ): MonoChromatic {
+    const hsl = RGBToHSL(rgb.r, rgb.g, rgb.b);
+
     let darkL = hsl[2];
     let lightL = hsl[2];
-    
+
     const darkVals: Array<number> = [];
     const lightVals: Array<number> = [];
 
     let i = 0;
 
-    while(i < colors) {
-
+    while (i < numOfColors) {
       const offset = Math.round(darkL * (percent / 100));
-      
+
       darkL = darkL - offset;
       lightL = lightL + offset;
 
       darkVals.push(darkL);
       lightVals.push(lightL);
 
-      i++
-
+      i++;
     }
 
-    const monoChromaticPalette: MonoChromatic = {light: [], dark: [], original: {r: rgb.r, g: rgb.g, b: rgb.b, a: 255}};
+    const monoChromaticPalette: MonoChromatic = {
+      light: [],
+      dark: [],
+      original: { r: rgb.r, g: rgb.g, b: rgb.b, a: 255 },
+    };
 
     darkVals.forEach((n) => {
-
       const rgb = HSLToRGB(hsl[0], hsl[1], n);
 
-      monoChromaticPalette.dark.push(
-        {
-          r: rgb[0],
-          g: rgb[1],
-          b: rgb[2],
-          a: 255,
-        }
-      );
-
+      monoChromaticPalette.dark.push({
+        r: rgb[0],
+        g: rgb[1],
+        b: rgb[2],
+        a: 255,
+      });
     });
 
     lightVals.forEach((n) => {
-
       const rgb = HSLToRGB(hsl[0], hsl[1], n);
 
-      monoChromaticPalette.light.push(
-        {
-          r: rgb[0],
-          g: rgb[1],
-          b: rgb[2],
-          a: 255,
-        }
-      );
-
+      monoChromaticPalette.light.push({
+        r: rgb[0],
+        g: rgb[1],
+        b: rgb[2],
+        a: 255,
+      });
     });
 
     return monoChromaticPalette;
-
-  }
-  
-
+  },
 };
+
+//////////////////////////////////
+/////// HELPERS /////////////////
+////////////////////////////////
 
 const RGBToHSL = (r: number, g: number, b: number): Array<number> => {
   r /= 255;
@@ -202,7 +271,11 @@ const HSLToRGB = (h: number, s: number, l: number): Array<number> => {
   const a = s * Math.min(l, 1 - l);
   const f = (n: number) =>
     l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-  return [Math.round(255 * f(0)), Math.round(255 * f(8)), Math.round(255 * f(4))];
+  return [
+    Math.round(255 * f(0)),
+    Math.round(255 * f(8)),
+    Math.round(255 * f(4)),
+  ];
 };
 
 const sortByDominantColor = (d: RGBARecord[], key: string): void => {
@@ -214,27 +287,31 @@ const sortByDominantColor = (d: RGBARecord[], key: string): void => {
 const getRGBValues = (d: Uint8ClampedArray): RGBARecord[] => {
   const rgbaVals: RGBARecord[] = [];
   for (let i = 0; i < d.length; i += 4) {
-    rgbaVals.push(
-      {
-        r: d[i],
-        g: d[i + 1],
-        b: d[i + 2],
-        a: 255,
-      }
-    );
+    rgbaVals.push({
+      r: d[i],
+      g: d[i + 1],
+      b: d[i + 2],
+      a: 255,
+    });
   }
   return rgbaVals;
 };
 
-const quantizeRGB = (d: RGBARecord[], startingDepth = 0, maxDepth = 2): RGBARecord[] => {
-  if(startingDepth === maxDepth) {
-    const color = d.reduce((p, c): RGBARecord => {
-      p.r += c.r;
-      p.g += c.g;
-      p.b += c.b;
-      return p;
-    }, { r: 0, g: 0, b: 0, a: 255 });
-
+const quantizeRGB = (
+  d: RGBARecord[],
+  startingDepth = 0,
+  maxDepth = 2
+): RGBARecord[] => {
+  if (startingDepth === maxDepth) {
+    const color = d.reduce(
+      (p, c): RGBARecord => {
+        p.r += c.r;
+        p.g += c.g;
+        p.b += c.b;
+        return p;
+      },
+      { r: 0, g: 0, b: 0, a: 255 }
+    );
 
     return [
       {
@@ -242,29 +319,30 @@ const quantizeRGB = (d: RGBARecord[], startingDepth = 0, maxDepth = 2): RGBAReco
         g: Math.round(color.g / d.length),
         b: Math.round(color.b / d.length),
         a: 255,
-      }
-    ]
-
+      },
+    ];
   }
 
   const dominantColor = findDominantColorRange(d);
-  
+
   sortByDominantColor(d, dominantColor);
 
   const middle = d.length / 2;
 
   return [
     ...quantizeRGB(d.slice(0, middle), startingDepth + 1, maxDepth),
-    ...quantizeRGB(d.slice(middle), startingDepth + 1, maxDepth)
-  ]
-
+    ...quantizeRGB(d.slice(middle), startingDepth + 1, maxDepth),
+  ];
 };
 
 const findDominantColorRange = (rgbaRecords: RGBARecord[]): string => {
   const d = rgbaRecords[0];
-  let rMin = d.r, rMax = d.r;
-  let gMin = d.g, gMax = d.g;
-  let bMin = d.b, bMax = d.b;
+  let rMin = d.r,
+    rMax = d.r;
+  let gMin = d.g,
+    gMax = d.g;
+  let bMin = d.b,
+    bMax = d.b;
 
   rgbaRecords.forEach((pixel): void => {
     rMin = Math.min(rMin, pixel.r);
@@ -281,11 +359,11 @@ const findDominantColorRange = (rgbaRecords: RGBARecord[]): string => {
   const bRange = bMax - bMin;
 
   const maxRange = Math.max(rRange, gRange, bRange);
-  
+
   if (maxRange === rRange) {
     return "r";
   } else if (maxRange === gRange) {
-  return "g";
+    return "g";
   }
   return "b";
 };
